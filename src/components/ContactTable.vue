fd2fd4a18c3e8528f7b4a1fd8d272e4491ba7e8f
Mka91 <kiwogens@yandex.ru>
2022-01-31 16:41:38 +0700

    ContactBook

---
+++ src/components/ContactTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>№</th>
+      <th>Name</th>
+      <th>User Name</th>
+      <th>Email</th>
+      <th>Address</th>
+      <th>Company</th>
+      <th>Phone</th>
+      <th>Website</th>
+      <th>Edit</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="(contact, index) in $store.state.contacts" :key="contact.id">
+      <template>
+        <td v-text="index + 1"></td>
+        <td v-text="contact.name"></td>
+        <td v-text="contact.username"></td>
+        <td v-text="contact.email"></td>
+        <td v-text="contact.address.city + ', ' + contact.address.streetA"></td>
+        <td v-text="contact.company.name"></td>
+        <td v-text="contact.phone"></td>
+        <td v-text="contact.website"></td>
+        <td>
+          <button class="btn btn-warning" type="button">Edit
+          </button>
+        </td>
+      </template>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'ContactTable',
+  created () {
+    this.$store.dispatch('loadDate')
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    contacts: []
   },
   mutations: {
+    loadDate (state) {
+      axios.get('https://demo.sibers.com/users')
+        .then(response => {
+          state.contacts = response.data
+        })
+    }
   },
   actions: {
+    loadDate (context) {
+      context.commit('loadDate')
+    }
   },
   modules: {
   }
